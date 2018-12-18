8e614244f882195db242f5be4cd57c638df54296
pbngrape <45889391+pbngrape@users.noreply.github.com>
2018-12-17 21:08:10 -0600

    Added Basic template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("hello, wrld")
+    template = loader.get_template('index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
